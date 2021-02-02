4af274bf9158d0db505c5fb40afe8166d652971d
Park Si Yeon <parksiyeon1020@gmail.com>
2021-02-02 13:03:41 +0900

    카메라, 큐브 이동 구현 (수정 필요)

---
+++ Etalo and Allin/Assets/Scripts/camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    [SerializeField]
+    Define.CameraMode mode = Define.CameraMode.QuaterView; // Define 스크립트에 있는 enum 중 하나
+    Vector3 delta;
+    public Transform player;
+
+    private Transform cameraTransform;
+
+
+    void Start()
+    {
+        cameraTransform = GetComponent<Transform>();
+            
+    }
+
+    void LateUpdate()
+    {
+        if (mode == Define.CameraMode.QuaterView)
+        {
+            cameraTransform.position = new Vector3(player.position.x - 0.52f, cameraTransform.position.y, player.position.z - 6.56f);
+            //transform.position = player.transform.position + delta;
+            transform.LookAt(player.transform);
+        }
+    }
+}
---
+++ Etalo and Allin/Assets/Scripts/cubeBasic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeBasic : MonoBehaviour
+{
+    //public Transform tranformCube;
+    [SerializeField]
+    private float walkSpeed;
+    private float lookSensitive; // 카메라 감도
+    private Rigidbody myRigid; 
+
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        float moveDirX = Input.GetAxis("Horizontal");
+        float moveDirZ = Input.GetAxis("Vertical");
+        Vector3 moveHorizontal = transform.right * moveDirX; //좌우 이동
+        Vector3 moveVertical = transform.forward * moveDirZ; //앞뒤 이동
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed; // 속도
+
+        myRigid.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+}
