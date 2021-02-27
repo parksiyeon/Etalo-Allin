0c26fcf7b9b77c4d3a6554015832ae2f5c09bed2
Park Si Hyeon <sh_2n@naver.com>
2021-02-27 16:01:15 +0900

    Player1 Moving & Room Manager
    Player Controller
    Room Manager
    Ground Check
    New Camera Setting

---
+++ Etalo and Allin/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+	PhotonView PV;
+
+	GameObject controller;
+
+	void Awake()
+	{
+		PV = GetComponent<PhotonView>();
+	}
+
+	void Start()
+	{
+		if (PV.IsMine)
+		{
+			CreateController();
+		}
+	}
+
+	void CreateController()
+	{
+	
+		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Etalo_body"), Vector3.zero, Quaternion.identity);
+	}
+
+	/*	public void Die()
+		{
+			PhotonNetwork.Destroy(controller);
+			CreateController();
+		}*/
+}
